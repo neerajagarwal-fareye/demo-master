32826c6a5b9f643cb95e11f56c467ea803a9db65
neerajfareye <neeraj.agarwal@getfareye.com>
2022-02-15 18:12:49 +0530

    basic mobile script

--- src/test/java/TestCases/MobileTest.java
+++ src/test/java/TestCases/MobileTest.java
@@ -23,8 +23,8 @@ public class MobileTest {
         cap.setCapability("udid","RZ8NA1CQA9V");
         cap.setCapability("platformName","Android");
         cap.setCapability("platformVersion","11");
-        cap.setCapability("appPackage", "com.myos.calculator2");
-        cap.setCapability("appActivity","com.myos.calculator2.calculator");
+        cap.setCapability("appPackage", "com.sec.android.app.popupcalculator");
+        cap.setCapability("appActivity","com.sec.android.app.popupcalculator.Calculator");
         URL url = null;
         try {
             url = new URL("http://127.0.0.1:4723/wd/hub");
@@ -34,6 +34,18 @@ public class MobileTest {
         appiumDriver = new AppiumDriver<MobileElement>(url,cap);
 
         System.out.println("application Started");
+        appiumDriver.findElementById("com.sec.android.app.popupcalculator:id/calc_keypad_btn_07").click();
+        appiumDriver.findElementById("com.sec.android.app.popupcalculator:id/calc_keypad_btn_add").click();
+        appiumDriver.findElementById("com.sec.android.app.popupcalculator:id/calc_keypad_btn_08").click();
+        appiumDriver.findElementById("com.sec.android.app.popupcalculator:id/calc_keypad_btn_equal").click();
+
+        System.out.println("Result --"+ appiumDriver.findElementById("com.sec.android.app.popupcalculator:id/calc_edt_formula").getText());
+
+        if(appiumDriver.findElementById("com.sec.android.app.popupcalculator:id/calc_edt_formula").getText().equals("15")){
+            System.out.println("test pass");
+        }else {
+            System.out.println("test fail");
+        }
 
     }
 }
